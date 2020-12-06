dc42f279628c0b65c2e9e5401d90513801465233
Lockett Pundt <kalifornium@hotmail.com>
2020-12-06 00:46:22 -0500

    feat(Index): :sparkles: imported setting entity and connected typeORM to the local db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
+import "reflect-metadata";
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express'
+import bodyParser from 'body-parser'
 import cors from "cors";
-import { PORT } from '../config'
+
+import { createConnection } from "typeorm";
+import { PORT, NODE_ENV, DB_NAME_LOCAL, DB_PASSWORD, DB_USERNAME, DB_PRODUCTION_URL } from '../config'
+import { Setting } from './entity/Setting'
+
+createConnection({
+  type: "postgres",
+  host: NODE_ENV === 'production' ? DB_PRODUCTION_URL : 'localhost',
+  port: 5432,
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME_LOCAL,
+  entities: [
+    Setting,
+  ],
+  synchronize: true,
+  logging: false
+}).then(async connection => {
+  console.log(`connection ---->`)
+  connection.runMigrations()
+}).catch(error => console.log(error));
+
 
 const app = express();
-app.use(cors());
+
+
 // add apollo server.
 // add body parser.
+
 
 app.get('/', (req, res) => res.send('yay!'));
 app.listen(PORT, () => {
