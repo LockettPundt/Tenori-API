d0cb018475620cd1ce889dbc2f4c4170dd7efa9a
Lockett Pundt <kalifornium@hotmail.com>
2021-05-11 23:11:46 -0400

    feat(ormconfig): :recycle: removed all the connection config from index

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,14 @@
 import "reflect-metadata";
 import { ApolloServer, makeExecutableSchema } from 'apollo-server';
-import bodyParser from 'body-parser'
 import { settingsResolvers, settingsType } from './schema/settings-type'
 import { createConnection } from "typeorm";
-import { PORT, NODE_ENV, DB_NAME_LOCAL, DB_PASSWORD, DB_USERNAME, DB_PRODUCTION_URL, DATABASE_URL } from '../config'
-import { Setting } from './entity/Setting'
+import { PORT, NODE_ENV } from '../config'
 
 if (!NODE_ENV) {
   process.env.NODE_ENV = 'development'
 }
 
-const isDev = process.env.NODE_ENV === 'development'
-
-createConnection({
-  type: "postgres",
-  port: 5432,
-  url: isDev ? undefined : DATABASE_URL,
-  host: isDev ? 'localhost' : undefined,
-  database: isDev ? DB_NAME_LOCAL : undefined,
-  ssl: !isDev ? {
-    rejectUnauthorized: false,
-  } : undefined,
-  entities: [
-    Setting,
-  ],
-  synchronize: true,
-  logging: false
-})
+createConnection()
   .then(async connection => {
     console.log(`connected to postgres!`)
   }).catch(error => console.log(error));
