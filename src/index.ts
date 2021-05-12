a6713edeec27fd5d2b86cc90fda492d174c4aacf
Lockett Pundt <kalifornium@hotmail.com>
2021-05-11 23:57:15 -0400

    refactor(ormconfig): :sparkles: went back to the json version...

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,30 @@ import "reflect-metadata";
 import { ApolloServer, makeExecutableSchema } from 'apollo-server';
 import { settingsResolvers, settingsType } from './schema/settings-type'
 import { createConnection } from "typeorm";
-import { PORT, NODE_ENV } from '../config'
+import { PORT, NODE_ENV, DB_NAME_LOCAL, DB_PASSWORD, DB_USERNAME, DB_PRODUCTION_URL, DATABASE_URL } from '../config'
+import { Setting } from './entity/Setting'
 
 if (!NODE_ENV) {
   process.env.NODE_ENV = 'development'
 }
 
-createConnection()
+const isDev = process.env.NODE_ENV === 'development'
+
+createConnection({
+  type: "postgres",
+  port: 5432,
+  url: isDev ? undefined : DATABASE_URL,
+  host: isDev ? 'localhost' : undefined,
+  database: isDev ? DB_NAME_LOCAL : undefined,
+  ssl: !isDev ? {
+    rejectUnauthorized: false,
+  } : undefined,
+  entities: [
+    Setting,
+  ],
+  synchronize: true,
+  logging: false
+})
   .then(async connection => {
     console.log(`connected to postgres!`)
   }).catch(error => console.log(error));
