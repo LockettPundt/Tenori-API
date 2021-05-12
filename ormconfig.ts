7baebd76d66ffeb96e10d4048504d2f646c5ae3b
Lockett Pundt <kalifornium@hotmail.com>
2021-05-11 23:10:58 -0400

    refactor(ormconfig): :recycle: changed the config file from json to .ts.

---
+++ ormconfig.ts
@@ -0,0 +1,31 @@
+import { DB_NAME_LOCAL, DB_PASSWORD, DB_USERNAME, DB_PRODUCTION_URL, DATABASE_URL } from './config'
+import { Setting } from './src/entity/Setting'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+export default {
+   type: "postgres",
+   port: 5432,
+   url: isDev ? undefined : DATABASE_URL,
+   host: isDev ? 'localhost' : undefined,
+   database: isDev ? DB_NAME_LOCAL : undefined,
+   ssl: !isDev ? {
+      rejectUnauthorized: false,
+   } : undefined,
+   entities: [
+      Setting
+   ],
+   migrations: [
+      "src/migration/**/*.ts"
+   ],
+   subscribers: [
+      "src/subscriber/**/*.ts"
+   ],
+   cli: {
+      entitiesDir: "src/entity",
+      migrationsDir: "src/migration",
+      subscribersDir: "src/subscriber"
+   },
+   synchronize: true,
+   logging: false
+}
